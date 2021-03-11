88a3e1fb9de3011b3f174dce37eeec41ffe80102
JillKlatt <klatt.jill@gmail.com>
2021-03-11 16:05:45 -0500

    commit tree 8a89275ca1379e4415218834eeeecbd9a8913bf5

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/' do
+        "Hello, World!"
+    end
+
+    get '/name' do
+        "My name is Jill Klatt!"
+    end
+
+    get '/hometown' do
+        "My hometown is Cleveland, Ohio!"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is Escape by Rupert Holmes!"
+    end
 end
